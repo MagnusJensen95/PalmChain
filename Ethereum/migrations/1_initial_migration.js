afa3951c1b887b416c0c103b89895be3884b15e3
Magnus Jensen <mag@it-minds.dk>
2018-10-21 15:58:03 +0200

    Events pt 2

--- Ethereum/migrations/1_initial_migration.js
+++ Ethereum/migrations/1_initial_migration.js
@@ -1,9 +1,9 @@
 var Migrations = artifacts.require("./Migrations.sol");
 var Consortium = artifacts.require("./Consortium.sol");
-var ConsortiumDeployer = artifacts.require("./ConsortiumDeployer.sol");
 
-module.exports = function(deployer) {
+
+module.exports = function (deployer) {
   deployer.deploy(Migrations);
   deployer.deploy(Consortium);
-  deployer.deploy(ConsortiumDeployer);
+
 };
--- Ethereum/test/Consortium.js
+++ Ethereum/test/Consortium.js
@@ -1,24 +1,25 @@
 const Consortium = artifacts.require("Consortium");
 
 contract('Consortium functionality test', async (accounts) => {
+    let instance;
+    let rspoAdmin;
+    beforeEach(async () => {
+        instance = await Consortium.deployed();
+        rspoAdmin = await instance.RSPOAdministrator.call();
+    })
 
-                it("Should assign an RSPO Administrator", async () => {
-                    let instance = await Consortium.deployed();
-                    let admin = await instance.getBalance.call(accounts[0]);
-                    assert.equal(balance.valueOf(), 10000);
-                })
+    it("Should assign an RSPO Administrator", async () => {
 
-                it("should call a function that depends on a linked library", async () => {
-                    let meta = await MetaCoin.deployed();
-                    let outCoinBalance = await meta.getBalance.call(accounts[0]);
-                    let metaCoinBalance = outCoinBalance.toNumber();
-                    let outCoinBalanceEth = await meta.getBalanceInEth.call(accounts[0]);
-                    let metaCoinEthBalance = outCoinBalanceEth.toNumber();
-                    assert.equal(metaCoinEthBalance, 2 * metaCoinBalance);
+        assert.equal(rspoAdmin, accounts[0]);
+    })
 
-                });
+    it("should call a function that depends on a linked library", async () => {
 
-                it("should send coin correctly", async () => {
+        assert.equal(1, 1);
+
+    });
 
 
-                })
+
+
+})
