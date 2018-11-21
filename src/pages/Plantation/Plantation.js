d47da091ff106d8fbdac49cc2887b691b8fcab57
Magnus <magnuskollip@hotmail.dk>
2018-11-21 21:18:08 +0100

    small fix on checking plantation address

--- src/pages/Plantation/Plantation.js
+++ src/pages/Plantation/Plantation.js
@@ -2,35 +2,66 @@ import React, { Component } from "react";
 
 import { connect } from "react-redux";
 import TopBar from "../../components/TopBar/TopBar";
-import { submitFFBToken, fetchPlantationInformation, identifyPlantationAddressByOwner, requestConsortiumApproval } from "../../store/actions/plantation";
+import {
+  submitFFBToken,
+  fetchPlantationInformation,
+  identifyPlantationAddressByOwner,
+  requestConsortiumApproval
+} from "../../store/actions/plantation";
 import { isZeroAddress } from "../../utils/mappings";
 import { plantationOwner } from "../../store/models/authentication";
 import { Button } from "@material-ui/core";
 
 export class Plantation extends Component {
-
   componentDidMount() {
+    console.log(this.props.plantationAddress);
     if (this.props.userAuthType !== plantationOwner) {
       return;
     }
-    if (isZeroAddress(this.props.plantationAddress)) {
-      this.props.onFetchPlantationAddress(this.props.userAddress, this.props.deployerAddress);
+    if (
+      isZeroAddress(this.props.plantationAddress) ||
+      this.props.plantationAddress == ""
+    ) {
+      this.props.onFetchPlantationAddress(
+        this.props.userAddress,
+        this.props.deployerAddress
+      );
       return;
     }
-    if (isZeroAddress(this.props.plantationAddress) || this.props.plantationAddress === undefined) {
+    if (
+      isZeroAddress(this.props.plantationAddress) ||
+      this.props.plantationAddress === undefined
+    ) {
       return;
     }
-    this.props.onFetchPlantationInformation(this.props.plantationAddress, this.props.userAddress)
-
+    this.props.onFetchPlantationInformation(
+      this.props.plantationAddress,
+      this.props.userAddress
+    );
   }
   render() {
     const requestApprovalButton = (
-      <Button variant="outlined" color="primary" onClick={() => this.props.onRequestApproval(this.props.plantationAddress, this.props.userAddress)}>Request Approval</Button>
-    )
+      <Button
+        variant="outlined"
+        color="primary"
+        onClick={() =>
+          this.props.onRequestApproval(
+            this.props.plantationAddress,
+            this.props.userAddress
+          )
+        }
+      >
+        Request Approval
+      </Button>
+    );
     return (
       <div>
         <TopBar title={"Plantation Overview"} />
-        {this.props.userAuthType === plantationOwner ? requestApprovalButton : <></>}
+        {this.props.userAuthType === plantationOwner ? (
+          requestApprovalButton
+        ) : (
+          <></>
+        )}
       </div>
     );
   }
@@ -45,16 +76,16 @@ const mapStateToProps = state => ({
   plantationInformation: state.plantationReducer.plantationProperties
 });
 
-
 //Fetching approval status may be redundant, as the info lies in the plantation reducer already
 const mapDispatchToProps = dispatch => ({
-  onFetchPlantationInformation: (plantationAddress, userAddress) => dispatch(fetchPlantationInformation(plantationAddress, userAddress)),
-  onSubmitToken: (plantationAddress, tokenObject, userAddress) => dispatch(submitFFBToken(tokenObject, userAddress)),
-  onFetchPlantationAddress: (userAddress, deployerAddress) => dispatch(identifyPlantationAddressByOwner(userAddress, deployerAddress)),
-  onRequestApproval: (plantationAddress, userAddress) => dispatch(requestConsortiumApproval(plantationAddress, userAddress))
-
-
-
+  onFetchPlantationInformation: (plantationAddress, userAddress) =>
+    dispatch(fetchPlantationInformation(plantationAddress, userAddress)),
+  onSubmitToken: (plantationAddress, tokenObject, userAddress) =>
+    dispatch(submitFFBToken(tokenObject, userAddress)),
+  onFetchPlantationAddress: (userAddress, deployerAddress) =>
+    dispatch(identifyPlantationAddressByOwner(userAddress, deployerAddress)),
+  onRequestApproval: (plantationAddress, userAddress) =>
+    dispatch(requestConsortiumApproval(plantationAddress, userAddress))
 });
 
 export default connect(
--- src/store/actions/plantation.js
+++ src/store/actions/plantation.js
@@ -1,121 +1,102 @@
 import {
-    SET_TOKENS_SUBMITTED,
-    SET_CURRENT_PLANTATION,
-    SET_PLANTATION_INFORMATION
-
+  SET_TOKENS_SUBMITTED,
+  SET_CURRENT_PLANTATION,
+  SET_PLANTATION_INFORMATION
 } from "./types";
 
-
-import { consortiumDeployer, deployNewConsortiumDeployer } from '../../utils/contractDeploymentInstance';
-import web3 from '../../utils/getWeb3';
-import consortiumInstance from '../../utils/consortiumInstance';
+import { consortiumDeployer } from "../../utils/contractDeploymentInstance";
 import { plantationInstance } from "../../utils/plantationInstance";
-import { mapToPlantation } from "../../utils/mappings";
-
-
-
-
-
+import { mapToPlantation, isZeroAddress } from "../../utils/mappings";
 
 export const setTokensSubmittedFromPlantation = tokens => {
-    return {
-        type: SET_TOKENS_SUBMITTED,
-        tokens: tokens
-    };
+  return {
+    type: SET_TOKENS_SUBMITTED,
+    tokens: tokens
+  };
 };
 
 export const setSelectedPlantation = plantationAddress => {
-    return {
-        type: SET_CURRENT_PLANTATION,
-        plantationAddress: plantationAddress
-    };
+  return {
+    type: SET_CURRENT_PLANTATION,
+    plantationAddress: plantationAddress
+  };
 };
 
 export const setPlantationProperties = plantationProperties => {
-    return {
-        type: SET_PLANTATION_INFORMATION,
-        plantationProperties: plantationProperties
-    };
+  return {
+    type: SET_PLANTATION_INFORMATION,
+    plantationProperties: plantationProperties
+  };
 };
 
 //fetch tokens submitted by this plantation
-export const fetchTokensSubmitted = (address) => {
-    return async dispatch => {
+export const fetchTokensSubmitted = address => {
+  return async dispatch => {};
+};
 
+//Fetch plantation address belonging to currently signed in user (if he is owner of a plantation)
+export const identifyPlantationAddressByOwner = (
+  userAddress,
+  deployerAddress
+) => {
+  return async dispatch => {
+    let deployer = consortiumDeployer(deployerAddress);
+    if (deployer === undefined) {
+      return;
     }
-}
 
-//Fetch plantation address belonging to currently signed in user (if he is owner of a plantation) 
-export const identifyPlantationAddressByOwner = (userAddress, deployerAddress) => {
-    return async dispatch => {
+    let plantationAddress = await deployer.methods
+      .plantationOwnerRelation(userAddress)
+      .call({
+        from: userAddress
+      });
 
-        let deployer = consortiumDeployer(deployerAddress);
-        if (deployer === undefined) {
-            return;
-        }
-
-        let plantationAddress = await deployer.methods.plantationOwnerRelation(userAddress).call({
-            from: userAddress
-        });
-
-
-        let plantation = plantationInstance(plantationAddress);
-        let information = await plantation.methods.getPlantationInformation().call({
-            from: userAddress
-        });
-        information["address"] = plantationAddress;
-
-
-        information = mapToPlantation(information);
-
-
-        dispatch(setPlantationProperties(information));
-        dispatch(setSelectedPlantation(plantationAddress));
+    if (isZeroAddress(plantationAddress)) {
+      return;
     }
-}
+    let plantation = plantationInstance(plantationAddress);
 
+    let information = await plantation.methods.getPlantationInformation().call({
+      from: userAddress
+    });
+    information["address"] = plantationAddress;
 
-//Fetch whether plantation is approved or pending approvel / has not sent approval request. 
+    information = mapToPlantation(information);
+
+    dispatch(setPlantationProperties(information));
+    dispatch(setSelectedPlantation(plantationAddress));
+  };
+};
+
+//Fetch whether plantation is approved or pending approvel / has not sent approval request.
 export const fetchPlantationInformation = (plantationAddress, userAddress) => {
-    return async dispatch => {
+  return async dispatch => {
+    let plantation = plantationInstance(plantationAddress);
+    let information = await plantation.methods.getPlantationInformation().call({
+      from: userAddress
+    });
+    information["address"] = plantationAddress;
 
+    information = mapToPlantation(information);
 
-        let plantation = plantationInstance(plantationAddress);
-        let information = await plantation.methods.getPlantationInformation().call({
-            from: userAddress
-        });
-        information["address"] = plantationAddress;
-
-
-        information = mapToPlantation(information);
-
-
-        dispatch(setPlantationProperties(information));
-    }
-}
-
+    dispatch(setPlantationProperties(information));
+  };
+};
 
 //Submit ffb token from selected user address
 export const submitFFBToken = (token, userAddress) => {
-    return async dispatch => {
-
-    }
-}
-
+  return async dispatch => {};
+};
 
 export const requestConsortiumApproval = (plantationAddress, userAddress) => {
-    return async dispatch => {
+  return async dispatch => {
+    let plantation = plantationInstance(plantationAddress);
+    plantation.methods.requestPlantationSubscription().send({
+      from: userAddress,
+      gas: 4712388,
+      gasPrice: 100000000000
+    });
 
-        let plantation = plantationInstance(plantationAddress);
-        plantation.methods.requestPlantationSubscription().send({
-            from: userAddress,
-            gas: 4712388,
-            gasPrice: 100000000000
-        });
-
-        dispatch(fetchPlantationInformation(plantationAddress));
-
-
-    }
-}
-
+    dispatch(fetchPlantationInformation(plantationAddress));
+  };
+};
